03c8ef0750b0230f4e49851da306055e9b50acdb
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-06-08 23:36:29 +0300

    fix import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './AboutComponent/about/about.component';
+import { AboutComponent } from './Components/AboutComponent/about/about.component';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 @NgModule({
