dd358b64e948662d295e2d07742c1bcc92fc3707
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-06-09 22:58:54 +0300

    create toggle button

---
+++ src/app/Components/toggle-button/toggle-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-button',
+  templateUrl: './toggle-button.component.html',
+  styleUrls: ['./toggle-button.component.css']
+})
+export class ToggleButtonComponent {
+
+}
