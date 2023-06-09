5a9284e81cd7562a2be87f44ae91fc82fffdf334
Yousef Adel <yousefjooo2018@gmail.com>
2023-06-09 05:43:48 +0300

    update main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './Components/AboutComponent/about/about.component';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { MainComponent } from './Components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
