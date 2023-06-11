c941bb153a4054ce79dc80724a07f7110905640d
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-06-11 21:02:56 +0300

    add toast in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastService } from './Services/toast-service.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hyre';
+  toastMessage!: string;
+  toastVisible=false;
+  toastSuccess!:boolean;
+  constructor(
+    private router: Router,
+    private toastService: ToastService,
+    
+    
+  ) {
+    // titleService.setTitle(this.title);
+    // iconSet singleton
+  }
+
+  ngOnInit(): void {
+    // this.router.events.subscribe((evt) => {
+    //   if (!(evt instanceof NavigationEnd)) {
+    //     return;
+    //   }
+    // });
+    
+    this.toastService.toastEvent.subscribe((data:any) => {
+        data=JSON.parse(data)
+        this.toastMessage = data.message;
+        this.toastSuccess=data.success;
+        console.log(this.toastMessage)
+        this.show()
+      });
+    
+  }
+  show(){
+    if(this.toastMessage.length==0){
+      return
+    }
+    this.toastVisible=true
+      setTimeout(()=>{
+        this.toastVisible=false
+        this.toastMessage=""
+      },4000)
+    }
+  
 }
