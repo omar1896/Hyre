0466edd134770ccb7a6d34a8c1fa9abdb2ed6c0a
Yousef Adel <yousefjooo2018@gmail.com>
2023-06-12 03:07:42 +0300

    make reusable dialog component

--- src/app/Components/dialog/dialog.component.ts
+++ src/app/Components/dialog/dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject, Input } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent {
-
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {title: string,body:string,cancelButton:string,nextButton:string}) { }
+  // @Input()title="";
+  // @Input() body="";
+  // @Input() cancelButton="";
+  // @Input() nextButton="";
 }
