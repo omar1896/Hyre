cb38c3559a6862e090371af4c679411b5ff4799a
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-06-16 19:30:22 +0300

    prepare ts class of it for the rest of work

--- src/app/Components/candidate/candidate.component.ts
+++ src/app/Components/candidate/candidate.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-candidate',
   templateUrl: './candidate.component.html',
   styleUrls: ['./candidate.component.css']
 })
-export class CandidateComponent {
+export class CandidateComponent implements OnInit {
+  candidates: any
+  constructor(){
+    this.candidates = [{id: 1 , name:"mohamed" , email: "13mohamed.yossef@gmail.com" , position:"Fullstack developer" , hasInterview: true}]
+  }
+  ngOnInit(): void {
+      // send http request to server to retrieve data of this.candidates
+  }
+
+  deleteCandidate(id: any){
+    // will send http req to delete this candidate
+  }
+
+  scheduleInterview(){
+
+  }
+
+  cancelInterview(){
+
+  }
 
 }
