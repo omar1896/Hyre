ac4d38a61030262c769b83546cd7535922f2a9d2
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-06-17 21:15:26 +0300

    add interview view and delete

--- src/app/Components/interviews/interviews.component.ts
+++ src/app/Components/interviews/interviews.component.ts
@@ -1,10 +1,37 @@
+import { InterviewService } from 'src/app/Services/interview.service';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-interviews',
   templateUrl: './interviews.component.html',
-  styleUrls: ['./interviews.component.css']
+  styleUrls: ['./interviews.component.css'],
 })
 export class InterviewsComponent {
+  interviews: any;
+  constructor(
+    private InterviewService: InterviewService,
+    public dialog: MatDialog,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.interviews = this.InterviewService.getInterviews().subscribe({
+      next: (response: any) => {
+        console.log(response.data);
+
+        this.interviews = response.data;
+      },
+    });
+  }
+
+  deleteInterview(id: any) {
+    console.log(id);
+    this.InterviewService.deleteInterview(id).subscribe({
+      next: (response: any) => {
+        this.interviews = response.data;
+      },
+    });
+  }
 }
