13caef4e4b2bb5dd6e5224154a753cfe58917754
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-06-18 22:14:29 +0300

    create function getSubscription to retrieve all info of them from server

--- src/app/Services/subscription.service.ts
+++ src/app/Services/subscription.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Environment } from '../../Environment/env';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionService {
 
-  constructor() { }
+  URL: string;
+  constructor(private http: HttpClient) {
+    this.URL = Environment.apiUrl ;
+  }
+
+  getSubscriptions() {
+    return this.http.get(this.URL);
+  }
 }
