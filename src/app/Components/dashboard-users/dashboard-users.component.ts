dbb6238bcd5a6247d362b18296ff32a8ff032834
omaralaa <omaralaa0989@gmail.com>
2023-06-19 23:54:26 +0300

    modify users table

--- src/app/Components/dashboard-users/dashboard-users.component.ts
+++ src/app/Components/dashboard-users/dashboard-users.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{AuthService} from 'src/app/Services/auth.service'
 
 @Component({
   selector: 'app-dashboard-users',
   templateUrl: './dashboard-users.component.html',
   styleUrls: ['./dashboard-users.component.css']
 })
-export class DashboardUsersComponent {
+export class DashboardUsersComponent implements OnInit {
 users:any
+
+constructor(private myClient:AuthService){
+
+
+}
+
+ngOnInit(): void {
+   this.users= this.myClient.getAllUsers().subscribe({
+    next: (data: any) => {
+      if (data.success) {
+        this.users = data["data"];
+        console.log(this.users)
+      }
+      else {
+        console.log(data.message);
+      }
+    }
+  });
+}
+
 }
