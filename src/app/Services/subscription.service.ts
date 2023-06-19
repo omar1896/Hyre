1e714690d800dd8a6ca4025cd7485cdb2864e36e
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-06-19 13:10:01 +0300

    create subscription service and create getallsub function in it

--- src/app/Services/subscription.service.ts
+++ src/app/Services/subscription.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Environment } from '../../Environment/env';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionService {
+  public channelWrapper: any;
 
   URL: string;
-  constructor(private http: HttpClient) {
-    this.URL = Environment.apiUrl ;
+  constructor(private http: HttpClient){
+    this.URL = Environment.apiUrl+'subscriptions'
   }
+
+
 
   getSubscriptions() {
     return this.http.get(this.URL);
