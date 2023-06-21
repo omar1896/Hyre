ce25bde9796ac6fb807a7e1f367ef80d6b452260
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-06-21 16:20:03 +0300

    thank you page

---
+++ src/app/Components/thankyou/thankyou.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thankyou',
+  templateUrl: './thankyou.component.html',
+  styleUrls: ['./thankyou.component.css']
+})
+export class ThankyouComponent {
+
+}
